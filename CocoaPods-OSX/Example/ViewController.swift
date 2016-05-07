a850977c051862a8d361fa292ebfe1324cd4e282
Victor Ilyukevich <ilyukevich.victor@gmail.com>
2016-05-07 15:24:35 -0700

    Example for OSX integration using CocoaPods

---
+++ CocoaPods-OSX/Example/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Victor Ilyukevich on 5/7/16.
+//  Copyright © 2016 Open Source. All rights reserved.
+//
+
+import Cocoa
+import FormatterKit
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    let locationFormatter = TTTLocationFormatter()
+    print(locationFormatter)
+
+    let colorFormatter = TTTColorFormatter()
+    print(colorFormatter)
+
+    let intervalFormatter = TTTTimeIntervalFormatter()
+    intervalFormatter.usesAbbreviatedCalendarUnits = true
+    print("\(intervalFormatter.stringForTimeInterval(-3600*24*7*3)) == 3 wk ago")
+  }
+
+}
+
